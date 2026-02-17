2f06129acc9e7627977fc8232dd84450a6c79b68
Ilaria Malavasi <ilariamala04@gmail.com>
2026-02-17 15:20:45 +0100

    agiunta lista todo

--- todo/src/todos/todos.ts
+++ todo/src/todos/todos.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todos',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './todos.css',
 })
 export class Todos {
-
+  todos: Todo[] = [
+    {
+    id: 1,
+    title: 'Studiare Angular',
+    completed: false,
+    },
+    {
+    id: 2,
+    title: 'Fare la spesa',
+    completed: false,
+    },
+    {
+    id: 3,
+    title: 'Portare fuori il cane',
+    completed: false,
+    }
+   ];
 }
